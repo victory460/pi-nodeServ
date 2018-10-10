a93d9e500255f02cd4601faaf067756577f0c5ab
victory460 <452962206@qq.com>
2018-10-10 17:05:49 +0800

    uart receive jiben ok

--- app.js
+++ app.js
@@ -22,12 +22,127 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function (req, res, next) {
+    // res.header('Access-Control-Allow-Origin', 'http://192.168.31.126:8100'); //之后这边主服务器设置个固定的外网IP或者域名
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8100'); //之后这边主服务器设置个固定的外网IP或者域名
+    res.header('Access-Control-Allow-Methods', 'POST');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.header('Access-Control-Allow-Credentials', true);
+    if (req.method == 'OPTIONS') {
+        res.sendStatus(200); /*让options请求快速返回*/
+    }
+    else {
+        next();
+    }
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-//-------------------------------------------------
 
+//------------------------
+Buffer.prototype.append = function(data){
+    data.copy(this,this.size,0,data.length)
+    this.size += data.length
+}
+
+Buffer.prototype.indexOf= function(data){
+    for(var i=0;i<this.size;i++){
+        j = 0
+        while(j<data.length){
+            if(data[j] == this[i+j]) j++
+            else break
+        }
+        if(j == data.length)
+            return i
+    }
+    return -1
+}
+
+Buffer.prototype.ltrim=function(N){
+    N = (N>this.size)? this.size : N
+    this.copy(this,0,N,this.size)
+    this.size -= N
+}
+//-------------------------------------------------
+    var serialOptions = {
+        portId : '/dev/ttyUSB0',
+        baudRate:9600,
+        dataBits:8,
+        stopBits:1,
+        // parity:PARITY_NONE
+
+    }
+    var serial = new Serial(serialOptions);
+     raspi.init(() => {
+        
+        console.log("seral new")
+        serial.open(() => {
+            serial.write('Hello from raspi-serial');
+            console.log('send messages')
+            serial.on('data', (data) => {
+            process.stdout.write(data);
+            });
+        });
+    });
+
+
+
+  let cmdBuf = []
+  let cmd = []
+  let recvData = []
+  let recvcmdBufComplete = false
+  let startFlag = false
+    serial.on('data',function receive(params) {
+        //  console.log(params)
+        // let startIndex = params.indexOf(0x7b)
+        // console.log(startIndex)
+
+
+
+
+        recvData = params.toString().split('')
+        //console.log("recvData = %s",recvData)
+        let startIndex = recvData.indexOf('{')
+        let endIndex = recvData.indexOf('}')
+        if (startIndex != -1) {
+            startFlag = true
+        }
+        if( startIndex!= -1  && endIndex == -1){
+           cmdBuf = cmdBuf.concat(recvData.slice(startIndex,recvData.length))
+            // console.log(cmdBuf)
+
+        }
+        else if (startFlag == true && endIndex == -1 ) {
+             cmdBuf = cmdBuf.concat(recvData)
+        }else if(startFlag == true && endIndex != -1){
+             cmdBuf = cmdBuf.concat(recvData.slice(0,endIndex+1))
+             startFlag = false
+             cmd =cmdBuf
+             cmdBuf = []
+             console.error("cmd=%s,",cmd.join(''))
+        }
+        else {
+            console.log('else')
+        }
+      
+        //  let printStr = cmdBuf.join("")
+        //     console.error(printStr)
+
+
+
+
+
+
+
+      
+    })
+    
+    app.get('/serial', function (req, res) {
+
+
+    res.end('serial');
+});
 
 //--------------------------------------------------
 
@@ -65,19 +180,6 @@ var storage = multer.diskStorage({
 var upload = multer({ storage: storage })
 
 
-app.use(function (req, res, next) {
-    // res.header('Access-Control-Allow-Origin', 'http://192.168.31.126:8100'); //之后这边主服务器设置个固定的外网IP或者域名
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8100'); //之后这边主服务器设置个固定的外网IP或者域名
-    res.header('Access-Control-Allow-Methods', 'POST');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.header('Access-Control-Allow-Credentials', true);
-    if (req.method == 'OPTIONS') {
-        res.sendStatus(200); /*让options请求快速返回*/
-    }
-    else {
-        next();
-    }
-});
 
 
 app.get('/api', function (req, res) {
